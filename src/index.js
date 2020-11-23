6d1225b410e5ff36de811af8ca662f652f0e8056
doomhammerhell <maiconrlyeh@gmail.com>
2020-11-23 14:53:23 -0300

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./styles.css";
-import Page from "./components/page";
-
-ReactDOM.render(<Page />, 
-    document.getElementById("root")
-);
